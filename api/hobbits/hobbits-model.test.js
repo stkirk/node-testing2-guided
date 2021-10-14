1ca5563d85c68074a954c12e0921229ee851283b
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-10-14 12:29:34 -0400

    wip

--- api/hobbits/hobbits-model.test.js
+++ api/hobbits/hobbits-model.test.js
@@ -1,9 +1,9 @@
-const db = require('.')
+const db = require('../../data/dbConfig')
 
 test('environment', () => {
   expect(process.env.NODE_ENV).toBe('testing')
 })
 
 beforeEach(async () => {
-  await db.migrate
+  await db.migrate.rollback()
 })
