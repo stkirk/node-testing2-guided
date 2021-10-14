4508c7c495ff619523b821da5076b0f34429c591
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-10-14 12:44:19 -0400

    wip

--- api/hobbits/hobbits-model.test.js
+++ api/hobbits/hobbits-model.test.js
@@ -20,7 +20,8 @@ test('environment', () => {
 
 describe('Hobbit.get()', () => {
   it('returns all (4) hobbits', async () => {
-    // 
+    // use the function to get data, store it in var
+    // assert that the length of the structure is 4
   })
   it.todo('hobbits returned are of the correct shape')
 })
