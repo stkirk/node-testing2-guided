90f1c5871c4a7a15ad1ddfcd8df83f46e2719597
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-10-14 13:17:22 -0400

    wip

--- api/hobbits/hobbits-model.test.js
+++ api/hobbits/hobbits-model.test.js
@@ -55,7 +55,7 @@ describe('Hobbit.insert(hobbit)', () => {
     // fetch hobbits
     // assert the inserted hobbit is there
     await Hobbit.insert({ name: 'bilbo' })
-    const hobbits = await Hobbit.getAll()
+    const hobbits = await Hobbit.getAll() // FRAGILITY!!!!!
     expect(hobbits).toHaveLength(5)
   })
   it.todo('inserting a hobbits resolves to the new hobbit')
