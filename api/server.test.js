5e697448e11aa3c2ff18e43d573ad9794f79ca08
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-10-14 13:27:14 -0400

    wip

--- api/server.test.js
+++ api/server.test.js
@@ -1,3 +1,16 @@
 const server = require('./server')
 const request = require('supertest')
-const db = request('./')
+const db = require('../data/dbConfig')
+
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+
+beforeEach(async () => {
+  await db.seed.run()
+})
+
+afterAll(async () => {
+  await db.destroy()
+})
